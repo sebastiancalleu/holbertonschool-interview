88c4df1a4898b92cd6b09ac6a49c911955080708
Sebastian <sebastian.calleu@gmail.com>
2021-12-09 23:26:59 -0500

    making executable and pep8

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
+
 def canUnlockAll(boxes):
     lt1 = []
-    for i in range (1, len(boxes)):
+    for i in range(1, len(boxes)):
         lt1.append(i)
 
-    for i in range (0, len(boxes)):
+    for i in range(0, len(boxes)):
         for j in range(0, len(boxes[i])):
             if boxes[i][j] in lt1:
                 lt1.remove(boxes[i][j])
