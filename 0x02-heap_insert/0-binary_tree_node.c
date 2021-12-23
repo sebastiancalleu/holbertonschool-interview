001cc3044116f5b649ca9103940f50f2a83ea715
Sebastian <sebastian.calleu@gmail.com>
2021-12-23 18:58:43 -0500

    adding file for first time.

---
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+* binary_tree_node - add new node to the binary tree
+*
+* @paren: tree
+* @value: int
+* Return: node
+*/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
+    binary_tree_t* newNode;
+
+    newNode = malloc(sizeof(binary_tree_t));
+    if (newNode == NULL) {
+        return NULL;
+    }
+
+    newNode->n = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    
+
+    if (parent == NULL){
+        newNode->parent = NULL;
+        return newNode;
+    } else {
+        newNode->parent = parent;
+        return newNode;
+    }
+
+}
