de1f5c904db574a19fa43bf7c5e5834263acb965
Sebastian <sebastian.calleu@gmail.com>
2021-12-23 22:39:58 -0500

    add file for first time.

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -2,17 +2,6 @@
 #include "binary_trees.h"
 
 /**
-* checkTop - check top 
-* @root: top
-* @newNode: new node
-* Return: node
-*/
-// heap_t *checkTop(heap_t *root, heap_t newNode) {
-
-// }
-
-
-/**
 * heap_insert - add new node to the binary tree
 *
 * @root: root
@@ -21,32 +10,32 @@
 */
 heap_t *heap_insert(heap_t **root, int value)
 {
-    heap_t *newNode;
+heap_t *newNode;
 
-    newNode = malloc(sizeof(heap_t));
-    if (newNode == NULL)
-    {
-        return NULL;
-    }
+newNode = malloc(sizeof(heap_t));
+if (newNode == NULL)
+{
+return (NULL);
+}
 
-    newNode->n = value;
+newNode->n = value;
 
-    if ((*root) == NULL)
-    {
-        newNode->left = NULL;
-        newNode->right = NULL;
-        newNode->parent = NULL;
-        *root = newNode;
-        return (newNode);
-    }
+if ((*root) == NULL)
+{
+newNode->left = NULL;
+newNode->right = NULL;
+newNode->parent = NULL;
+*root = newNode;
+return (newNode);
+}
 
-    if ((*root)->n < value)
-    {
-        newNode->left = *root;
-        (*root)->parent = newNode;
-        *root = newNode;
-        return (newNode);
-    }
+if ((*root)->n < value)
+{
+newNode->left = *root;
+(*root)->parent = newNode;
+*root = newNode;
+return (newNode);
+}
 
-    return (newNode);
+return (newNode);
 }
