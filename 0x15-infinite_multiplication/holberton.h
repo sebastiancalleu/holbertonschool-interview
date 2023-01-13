1faaa0836bd9cbc1fc4846d2812d7c17279b7d2b
Sebastian <sebastian.calleu@gmail.com>
2023-01-12 22:34:32 -0500

    adding files

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,11 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int _putchar(char c);
+
+#endif /* _HOLBERTON_H_ */
